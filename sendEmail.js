57f16b448620e7384710ba81b04ce9dd14ff7edc
Alvin <al.charity@gmail.com>
2021-06-28 03:03:54 -0400

    add type def, fix eerrors, rm comments

--- sendEmail.js
+++ sendEmail.js
@@ -56,6 +56,7 @@ const responsesUrl = ''
 */
 
 // run 'debugRunner()' when testing
+/** @returns {void} */
 function debugRunner() {
   /** @type {string} */
   const admin = undefined;
@@ -66,8 +67,8 @@ function debugRunner() {
   try {
     sendEmail(debug);
   } catch (e) {
-    var emsg = "Script ran into an error!\n\n" + e;
-    MailApp.sendEmail(admin, emsg.replace("\n\n" + e), emsg);
+    var emsg = `Script ran into an error!\n\n${e}`;
+    MailApp.sendEmail(admin, emsg.replace(`{\n\n${e}`), emsg);
 
     throw e;
   }
@@ -76,10 +77,15 @@ function debugRunner() {
 // processing parameters specific to the needs of this form
 // edit this function to update required 'data' parameters and
 // > add any helper scripts
+/** @returns { object} */
 function procParams(recipient, mailFooter) {
-  const activeSpreadsheet = SpreadsheetApp.getActiveSheet()
-  const sheetName = activeSpreadsheet.getName()
+  /** @type {object} */
+  const activeSpreadsheet = SpreadsheetApp.getActiveSheet();
 
+  /** @type {string} */
+  const sheetName = activeSpreadsheet.getName();
+
+  /** @returns {string} */
   const getLastColumnLetter = function getLastColumnLetter() {
     var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     var alphaLen = alphabet.length;
@@ -88,95 +94,112 @@ function procParams(recipient, mailFooter) {
     if (col <= alphaLen) return alphabet.splice(col - 1, 1).toString();
 
     var firstLetter = alphabet[Math.floor(col / alphaLen) - 1];
-    return firstLetter + alphabet.splice(col % alphaLen, 1).toString();
+    return `${firstLetter}${alphabet.splice(col % alphaLen, 1).toString()}`;
   };
-
 
   /** @type {object} */
   let executor = {
     data: {
       // admin will receive error notifications
+      /** @type {string} */
       admin: admin,
       // add any recipient names or code to the
       // > executor.data.recipient function below
-      // NOTE formName and sheetId will be extracted from 
+      // NOTE formName and sheetId will be extracted from
       // const formName and const sheet
-      formName: sheetName.replace(' (Responses'),
-      recipient: undefined,
+      formName: sheetName.replace(" (Responses"),
+      /** @type {string} */
+      recipient: recipient,
       // leave emailFooter blank if it is not needed
+      /** @type {string} */
       emailFooter: mailFooter,
       // NOTE sheetID is required for this script to work properly
-      // NOTE formName and sheetId will be extracted from 
+      // NOTE formName and sheetId will be extracted from
       // const formName and const sheet
-      sheetId:activeSpreadsheet.getSheetId() ,
+      /** @type {string} */
+      sheetId: activeSpreadsheet.getSheetId(),
       // used to extract the form name from the sheet name.
       // > the 'responses' default is typical for most forms
       sheetNameFilter: ` (Responses)`,
       // used to create the email subject from the sheet name.
       // @todo verify the steps to change the sheetNameFilter work properly
       // the text in the subjectFilter will be added to the sheet name
-      // to generate an email subject. if you do not want the additional 
+      // to generate an email subject. if you do not want the additional
       // text in the email title you can leave this section blank -- use ''
       // NOTE the modifications above have not yet been tested (as of 2/16/2021)
-      subjectFilter: ' Form Submission',
+      subjectFilter: " Form Submission",
       sheetInfo: {
         // if you wamt the sheet to start at a different column, enter
         // your desired column letter below.
-        firstCol: 'A',
+        firstCol: "A",
+        /** @type {string} */
         lastCol: getLastColumnLetter(),
-        lastRow: activeSpreadsheet.getLastRow()
+        /** @type {number} */
+        lastRow: activeSpreadsheet.getLastRow(),
       },
-    }
+    },
   };
 
-  executor.data.recipient = recipient
   return executor;
 }
 
 // main runner function. on form submit, execute sendEmail, end script.
 // sendEmail(true) to send all notifications to 'admin' for testing
+/** @returns {void} */
 function sendEmail(debug) {
   // create the helper object
+  /** @type {object} */
   var parameters = procParams();
 
   // extract helper code and info from procParams (var parameters)
   // assumption: parameters.helper contains an object of helper vars / funcs
   // var helper = parameters.helper;
 
+  /** @type {object} */
   var data = parameters.data;
-  var id =  data.sheetId;
+
+  /** @type {string} */
+  var id = data.sheetId;
 
   //  open the sheet for parsing
+  /** @type {object} */
   var sheet = SpreadsheetApp.openById(id);
 
   // sheetInfo = { firstCol: ..., lastCol: ... }
+  /** @type {object} */
   var sheetInfo = data.sheetInfo;
 
   // Get the latest response range as text
-  sheetInfo.rangeString = 
-    `${sheetInfo.firstCol}${sheetInfo.lastRow}:${sheetInfo.lastCol}${sheetInfo.lastRow}`
+  sheetInfo.rangeString = `${sheetInfo.firstCol}${sheetInfo.lastRow}:${sheetInfo.lastCol}${sheetInfo.lastRow}`;
 
   // use the response range to get the questions from row 1
   // if your questions are in a different row or column
   // change 'A1' to the location of the cell containing the first question
-  // replace the '1' in 'sheetInfo.lastCol with the number of the row 
-  // containing the last question. 
+  // replace the '1' in 'sheetInfo.lastCol with the number of the row
+  // containing the last question.
   sheetInfo.questionString = `A1:${sheetInfo.lastCol + 1}`;
 
   // get the values for question and latest response ranges
   // @todo could this be done in a better way? (prolly)
+  /** @type {Array} */
   sheetInfo.questions = sheet.getRange(sheetInfo.questionString).getValues()[0];
-  sheetInfo.submissionData = sheet.getRange(sheetInfo.rangeString).getValues()[0];
+
+  /** @type {Array} */
+  sheetInfo.submissionData = sheet
+    .getRange(sheetInfo.rangeString)
+    .getValues()[0];
 
   // only shorten timestamps if the timestamps array has a value
-  if (data.timestampsArray.join('')) {
+  if (data.timestampsArray.join("")) {
     for (var n in data.timestampsArray) {
       // tsi === time stamp index
       var tsi = data.timestampsArray[n];
 
       // change the date formatting if needed
       sheetInfo.submissionData[tsi] = Utilities.formatDate(
-        sheetInfo.submissionData[tsi], Session.getScriptTimeZone(), 'M/d/yyyy h:mm a'
+        sheetInfo.submissionData[tsi],
+        Session.getScriptTimeZone(),
+        "M/d/yyyy h:mm a"
       ).toString();
     }
   }
@@ -199,13 +222,13 @@ function sendEmail(debug) {
 
   // add styles for full table and nest the dataTable rows in table tags
   var tablestyle = 'style="width:80%;padding:7px;"';
-  dataTable = `<table ${tablestyle}>${dataTable.join('')}</tables>`
+  dataTable = `<table ${tablestyle}>${dataTable.join("")}</tables>`;
 
   // Use admin email address if debug is true, otherwise use the script default
-  var recipient = (debug === true) ? data.admin : data.recipient();
+  var recipient = debug === true ? data.admin : data.recipient();
 
   // create email subject
-  var sheetName = sheet.getName().replace(data.sheetNameFilter, '');
+  var sheetName = sheet.getName().replace(data.sheetNameFilter, "");
   var subject = sheetName + data.subjectFilter;
 
   // add a company logo if desired. otherwise, comment out the two lines below
@@ -214,12 +237,17 @@ function sendEmail(debug) {
   logo = `<img src="${logo}" width="120px" height="80px">`;
 
   // create the email body
-  var emailFooter = procParams.emailFooter;
+  var emailFooter = parameters.emailFooter;
 
-  var body = logo + `<br><br>
+  var body =
+    logo +
+    `<br><br>
     'Hello,<br><br>'${sheetName} form was submitted on ${sheetInfo.submissionData[0]}. 
     Please find the submitted information below.<hr><br>${dataTable}<br><br><br><br>${emailFooter}`;
 
   // send email to 'recipient', admin if debugging
-  MailApp.sendEmail(recipient, subject, body, { htmlBody: body, noReply: true });
+  MailApp.sendEmail(recipient, subject, body, {
+    htmlBody: body,
+    noReply: true,
+  });
 }
