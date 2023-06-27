4adef9552f7b845ab4d3c88bb73b028caa998f0e
unforswearing <al.charity@gmail.com>
2023-06-27 09:51:27 +0000

    Jun 27, 2023, 5:51 AM

--- sendEmail.js
+++ sendEmail.js
@@ -46,33 +46,33 @@ const getLastColumnLetter = function getLastColumnLetter() {
  * The parameters in this `procParams` object will default to the
  * imported config file, calculating information from the active
  * sheet only where necessary. 
- * @see config~config for usable properties.
-* @property {string} data.admin admin will receive error notifications
-* @property {string} data.formName 
-* @property {string} data.recipient 
-* add any recipient email addresses here. these may be single
-* addresses or an array of quoted addresses. 
-* @property {string} data.emailFooter
-* `emailFooter` specifies the html string to be used in the emails
-* that are sent from this script. Leave blank if you do not require
-* an email footer
-* @property {string} data.sheetId
-* Property `sheetID` is required for this script to work properly
-* Properties `formName` and `sheetId` will be extracted from
-* `const formName` and `const sheet`
-* @property {string} data.sheetNameFilter 
-* used to extract the form name from the sheet name.
-* the 'responses' default is typical for most forms.
-* @property {string} data.subjectFilter
-* `subjectFilter` is used to create the email subject from the sheet name.
-* the text in the subjectFilter will be added to the sheet name
-* to generate an email subject. if you do not want the additional
-* text in the email title you can leave this section blank -- use ''
-* NOTE the modifications above have not yet been tested (as of 2/16/2021)
-* @property {{firstCol: string, lastCol: string, lastRow: number}} data.sheetInfo
-* The first column is set to 'A' by default. To use a different first column
-* modify the `firstCol` parameter to another column in your sheet. 
-* */
+ * @property {string} data.admin admin will receive error notifications
+ * @property {string} data.formName 
+ * @property {string} data.recipient 
+ * add any recipient email addresses here. these may be single
+ * addresses or an array of quoted addresses. 
+ * @property {string} data.emailFooter
+ * `emailFooter` specifies the html string to be used in the emails
+ * that are sent from this script. Leave blank if you do not require
+ * an email footer
+ * @property {string} data.sheetId
+ * Property `sheetID` is required for this script to work properly
+ * Properties `formName` and `sheetId` will be extracted from
+ * `const formName` and `const sheet`
+ * @property {string} data.sheetNameFilter 
+ * used to extract the form name from the sheet name.
+ * the 'responses' default is typical for most forms.
+ * @property {string} data.subjectFilter
+ * `subjectFilter` is used to create the email subject from the sheet name.
+ * the text in the subjectFilter will be added to the sheet name
+ * to generate an email subject. if you do not want the additional
+ * text in the email title you can leave this section blank -- use ''
+ * NOTE the modifications above have not yet been tested (as of 2/16/2021)
+ * @property {{firstCol: string, lastCol: string, lastRow: number}} data.sheetInfo
+ * The first column is set to 'A' by default. To use a different first column
+ * modify the `firstCol` parameter to another column in your sheet. 
+ * @see config~config for source properties for `procParams{}`
+ * */
 let procParams = {
   data: {
     admin: config.admin,
