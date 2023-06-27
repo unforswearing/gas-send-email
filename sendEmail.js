94e730c49759b77f652cd9f668ec3644fea90928
unforswearing <al.charity@gmail.com>
2023-06-27 06:11:37 +0000

    Jun 27, 2023, 2:11 AM

--- sendEmail.js
+++ sendEmail.js
@@ -1,28 +1,10 @@
 // Repository: https://github.com/unforswearing/gas-send-email
-
-/**
-* @description
- * A simple send email function for [Google Apps Script](https://developers.google.com/apps-script/reference/) projects.
-*
-* Install via `npm`:
-* 
-* ```
-* npm install google-apps-script-send-email
-* ```
-*
-* This script performs the following actions
-* 
-* - Collects the latest form response from the responses spreadsheet
-* - Generates a template from the submitted information
-* - Sends an email to specified recipients
-* 
-* **Note:** The sendEmail.js file may need small modifications to work with your project. 
-*/
-
 import config from "./config";
 
 // run 'debugRunner()' when testing
 /**
+ * `debugRunner()` tests the `sendEmail()` function, 
+ * catching any errors that arise. 
  * @argument {string} admin
  * @returns {void}
  * */
