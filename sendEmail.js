b8f46d4bece809aa88582adac863d4092f5e46f1
unforswearing <al.charity@gmail.com>
2023-06-27 10:18:18 +0000

    Jun 27, 2023, 6:18 AM

--- sendEmail.js
+++ sendEmail.js
@@ -76,7 +76,7 @@ const getLastColumnLetter = function getLastColumnLetter() {
  * @property {{firstCol: string, lastCol: string, lastRow: number}} data.sheetInfo
  * The first column is set to 'A' by default. To use a different first column
  * modify the `firstCol` parameter to another column in your sheet. 
- * @see {@link emailConfig}
+ * Additional information about `const config`: {@link emailConfig}
  * */
 let procParams = {
   data: {
