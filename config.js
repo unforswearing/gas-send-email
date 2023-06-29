2f35d09849d8b1602f54692c1eb1b0d5fe579c43
unforswearing <al.charity@gmail.com>
2023-06-29 05:08:47 +0000

    Jun 29, 2023, 1:08 AM

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * The `config` object must be completed for the `sendMail` function 
  * to work correctly. This object is passed to {@link module:sendEmail~sendMail|the sendEmail function} with values from the current sheet environment via 
  * {@link module:sendEmail~procParams|the procParams object}.
- * 
+ * <br /><br />
  * Please see the documentation for the parameters in `const config` below. 
  * All parameter values are `undefined` by default. 
  * @author unforswearing
@@ -17,8 +17,9 @@
 * @property {string} admin admin will receive error notifications
 * @property {string} formName 
 * @property {string} recipient 
-* add any recipient email addresses here. these may be single
-* addresses or an array of quoted addresses. 
+* Add email addresses here. All form responses will be sent to any 
+* address added. Addresses may be for one individual or an array of 
+* addresses for multiple recipients. 
 * @property {string} emailFooter
 * `emailFooter` specifies the html string to be used in the emails
 * that are sent from this script. Leave blank if you do not require
@@ -28,14 +29,13 @@
 * Properties `formName` and `sheetId` will be extracted from
 * `const formName` and `const sheet`
 * @property {string} sheetNameFilter 
-* used to extract the form name from the sheet name.
+* `sheetNameFilter` is used to extract the form name from the sheet name.
 * the 'responses' default is typical for most forms.
 * @property {string} subjectFilter
 * `subjectFilter` is used to create the email subject from the sheet name.
 * the text in the subjectFilter will be added to the sheet name
 * to generate an email subject. if you do not want the additional
 * text in the email title you can leave this section blank -- use ''
-* NOTE the modifications above have not yet been tested (as of 2/16/2021)
 * @property {{firstCol: string, lastCol: string, lastRow: number}} sheetInfo
 * The first column is set to 'A' by default. To use a different first column
 * modify the `firstCol` parameter to another column in your sheet. 
